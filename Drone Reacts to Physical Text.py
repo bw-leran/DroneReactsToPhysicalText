8fb82586b4030abb2b2258aa406aa6731d75b99e
bw-leran <brandonmiller2@Live.com>
2020-09-25 14:15:59 -0700

    First commit

---
+++ Drone Reacts to Physical Text.py
@@ -0,0 +1,137 @@
+from PIL import Image
+from PIL import ImageFilter
+from easytello import tello
+from langdetect import detect
+import time
+import pytesseract
+import os
+import pyautogui
+import cv2
+import sys
+
+#program to have attach OCR reader to tello camera to create a software that is capable of reacting/following commands that are 
+#printed in text
+
+#public variables
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\Brandon Miller\\AppData\\Local\\Tesseract-OCR\\tesseract.exe'
+
+drone = tello.Tello()
+
+commandList = ['land','fly','up','down','left','right','forward','square','test','stop']
+commandListChin = ['落','起','上','下','左','右','前','后','方','验']
+
+#funcs and private variables
+def activateStream():
+
+    drone.streamon()
+    print('Streaming...')
+
+def deactivateStream():
+    drone.streamoff()
+    print('Stream Has Ended...')
+    False
+
+def activateOCR():
+
+    sc = pyautogui.screenshot(region=(114,137,958,716))
+    os.remove('C:\\Users\\Brandon Miller\\Documents\\Python Scripts\\WB\\raw_image.png')
+    sc.save('C:\\Users\\Brandon Miller\\Documents\\Python Scripts\\WB\\raw_image.png')
+    
+    try:
+        rawImage = Image.open('C:\\Users\\Brandon Miller\\Documents\\Python Scripts\\WB\\raw_image.png')
+        rawImage = rawImage.filter(ImageFilter.SHARPEN)
+        rawImage = rawImage.filter(ImageFilter.SHARPEN)
+        os.remove('C:\\Users\\Brandon Miller\\Documents\\Python Scripts\\WB\\raw_image.png')
+        sc.save('C:\\Users\\Brandon Miller\\Documents\\Python Scripts\\WB\\raw_image.png')
+        rawImage = cv2.imread('C:\\Users\\Brandon Miller\\Documents\\Python Scripts\\WB\\raw_image.png')
+        rawImage = cv2.cvtColor(rawImage,cv2.COLOR_BayerRG2GRAY)
+        detectedTextEng = pytesseract.image_to_string(rawImage)
+        detectedLangEng = detect(detectedTextEng)
+        rawImage.show()
+        try:
+            if detectedLangEng == 'en':
+                print('EN Detected')
+                commands = detectedTextEng.lower()
+                commands = commands.split()
+                print(commands)
+
+                for command in commands:
+                    if command in commandList:
+                        if command == 'land':
+                            drone.land()
+                        elif command == 'fly':
+                            drone.takeoff()
+                        elif command == 'up':
+                            drone.up(2)
+                        elif command == 'down':
+                            drone.down(2)
+                        elif command == 'right':
+                            drone.right(2)
+                        elif command == 'left':
+                            drone.left(2)
+                        elif command == 'forward':
+                            drone.forward(1)
+                        elif command == 'square':
+                            for i in range(4):
+                                drone.forward(4)
+                                drone.cw(3)
+                        elif command == 'test':
+                            print('Test Success')
+                        elif command == 'stop':
+                            deactivateStream()
+                        
+                        time.sleep(4)
+                        break
+
+                    else:
+                        pass
+            else:
+                detectedTextChin = pytesseract.image_to_string(rawImage, lang = 'chi_tra')
+                detectedLangChin = detect(detectedTextChin)
+                if detectedLangChin == 'zh-cn':
+                
+                    print('CM Detected')
+
+                    try:
+                        commands = detectedTextChin.split()
+                        print(commands)
+                    except:
+                        commands = detectedTextChin
+                        print(commands)
+
+                    for command in commands:
+                        if command in commandListChin:
+                            if command == '落':
+                                drone.land()
+                            elif command == '起':
+                                drone.takeoff()
+                            elif command == '上':
+                                drone.up(2)
+                            elif command == '下':
+                                drone.down(2)
+                            elif command == '右':
+                                drone.right(2)
+                            elif command == '左':
+                                drone.left(2)
+                            elif command == '前':
+                                drone.forward(1)
+                            elif command == '方':
+                                for i in range(4):
+                                    drone.forward(4)
+                                    drone.cw(3)
+                            elif command == '验':
+                                print('Test Success')
+                            else:
+                                pass
+        except:
+            pass
+
+    except:
+        pass
+
+if __name__ == '__main__':
+    drone.takeoff()
+    activateStream()
+    while True:
+        time.sleep(3)
+        activateOCR()
